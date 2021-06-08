3a708bd1fa7edccfa2f420d19123b0c280e60fd2
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-06-08 15:47:09 -0300

    criando store e reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,11 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = { email: '' };
+
+export default function user(state = initialState, action) {
+  switch (action.type) {
+  case '':
+    return state;
+  default:
+    return state;
+  }
+}
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,11 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = { currencies: [], expenses: [] };
+
+export default function wallet(state = initialState, action) {
+  switch (action.type) {
+  case '':
+    return state;
+  default:
+    return state;
+  }
+}
