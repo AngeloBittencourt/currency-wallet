8d1a473ae95e677d10c1cd3e4565295639d54255
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-06-10 11:02:56 -0300

    corrigindo proptypes

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 
 class Table extends Component {
   renderTable() {
@@ -11,20 +12,24 @@ class Table extends Component {
         <td>{expense.tag}</td>
         <td>{expense.method}</td>
         <td>{expense.value}</td>
-        <td>{expense.exchangeRates[expense.currency].name}</td>
+        <td>{expense.exchangeRates[expense.currency].name.split('/')[0]}</td>
         <td>{Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
         <td>
           {Number(expense.value * expense.exchangeRates[expense.currency].ask)
             .toFixed(2)}
         </td>
         <td>Real</td>
+        <td>
+          <button>Edit</button>
+          <button> Remove </button>
+        </td>
       </tr>
     )));
   }
 
   render() {
     return (
-      <table>
+      <table border="1" rules="ROWS">
         <thead>
           <tr>
             <th>Descrição</th>
@@ -38,7 +43,6 @@ class Table extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
-        <hr />
         <tbody>
           { this.renderTable() }
         </tbody>
@@ -46,6 +50,10 @@ class Table extends Component {
     );
   }
 }
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(Object).isRequired,
+};
 
 const mapStateToProps = ({ wallet: { expenses } }) => ({ expenses });
 
